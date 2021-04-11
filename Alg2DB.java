0f4b051303a6b25877f802982cf0ff6b7cf99b30
trishnguyen2001 <74442131+trishnguyen2001@users.noreply.github.com>
2021-04-11 12:32:01 -0700

    TestUpload

--- Alg2DB.java
+++ Alg2DB.java
@@ -1,5 +1,48 @@
 package d1;
 
-public class Alg2DB {
+import java.util.ArrayList;
 
+public class Alg2DB implements DatabaseHelper{
+	private int counter;
+	
+	private ArrayList<Data> db;
+	
+	public Alg2DB() {
+		counter = 0;
+		db = new ArrayList<>();
+	}
+
+	@Override
+	public void addData(Problem p) {
+		Data add = new Data(counter, p);
+		db.add(add);
+		counter++;
+	}
+
+	public Data search(int id) throws Exception{
+		for(Data d: db) {
+			if(id == d.id) {
+				return d;
+			}
+		}
+		throw new Exception("problem not found");
+	}
+
+	@Override
+	public void deleteData(int id) throws Exception{
+		Data result = search(id);
+		if(result.id > 0) {
+			db.remove(id);
+		}
+		throw new Exception("problem not found");
+	}
+
+	@Override
+	public Problem getProblem(int id) throws Exception {
+		Data result = search(id);
+		if(result.id > 0) {
+			return result.p;
+		}
+		throw new Exception("problem not found");
+	}
 }

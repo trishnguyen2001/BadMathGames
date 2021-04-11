1a38626c82aee52fcadb16fa78b7a97673dcfb4b
DanNguyen2001 <78176995+DanNguyen2001@users.noreply.github.com>
2021-04-11 11:11:54 -0700

    Add files via upload

---
+++ Scoreboard.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Scoreboard {
+    Score theScore;
+    //DataBaseHelper dbh;
+
+    public Scoreboard showScoreBoard(ArrayList<Score> s) {
+        return null;
+    }
+}

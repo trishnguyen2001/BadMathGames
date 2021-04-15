06ab27530cd80da674d301cce7ba3d1764edcf4a
trishnguyen2001 <74442131+trishnguyen2001@users.noreply.github.com>
2021-04-15 16:13:22 -0700

    Delete DatabaseHelper.java

--- DatabaseHelper.java
+++
@@ -1,9 +0,0 @@
-package d1;
-import java.util.ArrayList;
-
-public interface DatabaseHelper {
-	void addData(Problem p);
-	void deleteData(int id) throws Exception;
-	Problem getProblem(int id) throws Exception;
-	Data search(int id) throws Exception;
-}

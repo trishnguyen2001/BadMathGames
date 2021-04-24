08fcd0f23c8754cd2a5ee2f71867b97c6f71a056
trishnguyen2001 <74442131+trishnguyen2001@users.noreply.github.com>
2021-04-24 12:22:13 -0700

    Update Score.java

--- main/model/Score.java
+++ main/model/Score.java
@@ -1,9 +1,16 @@
 package main.model; 
 
-public class Score  {
+public class Score implements Comparable{
     int correct;
     String subject;
-
+    String player;
+    
+    public Score(String player, String subject, int correct) {
+    	this.player = player;
+    	this.correct = correct;
+        this.subject = subject;
+    }
+    
     public Score(String subject, int correct) {
         this.correct = correct;
         this.subject = subject;
@@ -13,4 +20,16 @@ public class Score  {
     	return correct;
     }
 
+	//compare by score first, then by name
+    //only compare two scores w/ the same subject
+    @Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+    
+	public String toString() {
+		return player + "," + subject + "," + correct;
+	}
+    
 }

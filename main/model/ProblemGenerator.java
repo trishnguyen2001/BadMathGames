23083c0148197c533e02b5653b55ec9867792c88
alannnguyen1 <74015995+alannnguyen1@users.noreply.github.com>
2021-04-28 15:23:08 -0700

    Update ProblemGenerator.java
    changed a 50 to a 10

--- main/model/ProblemGenerator.java
+++ main/model/ProblemGenerator.java
@@ -14,7 +14,7 @@ public class ProblemGenerator {
     }
 
     public void randomize(){
-        int max = 50; //50 problems in set
+        int max = 10; //10 problems in set
         int min = -1;
         
         for(int i= 0; i <= 20; i++) {

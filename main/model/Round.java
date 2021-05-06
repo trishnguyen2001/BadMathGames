98f0a3bce6f5a845956dbf10262c711b0fbde9da
Alan Nguyen <nguyenalan2000@gmail.com>
2021-05-05 17:44:30 -0700

    Added iterator to Round

--- main/model/Round.java
+++ main/model/Round.java
@@ -3,34 +3,55 @@ package main.model;
 import java.util.ArrayList;
 
 public class Round {
-	private ArrayList<Problem> problems;
-	private ProblemGenerator pg;
-	private int current;
+    private ArrayList<Problem> problems;
+    private ProblemGenerator pg;
+    private int current;
 
-	public Round() {
-		System.out.println("ROUND: round created");
-		current = 0;
-	}
+    public Round() {
+        System.out.println("ROUND: round created");
+        current = 0;
+    }
 
-	public void setProbGen(ProblemGenerator pg) {
-		this.pg = pg;
-		this.pg.randomize();
-		problems = pg.getSet();
-	}
+    public void setProbGen(ProblemGenerator pg) {
+        this.pg = pg;
+        this.pg.randomize();
+        problems = pg.getSet();
+    }
 
-	public Problem getNext() {
-		if(current < problems.size())
-		{
-			
-			Problem p = problems.get(current);
-			
-			
-			System.out.println("ROUND: current problem = " + p.getProblem());
-			
-			
-			current++;
-			return p;
-		}
-		return null;
-	}
+    public Problem getNext() {
+        if (current < problems.size()) {
+
+            Problem p = problems.get(current);
+
+
+            System.out.println("ROUND: current problem = " + p.getProblem());
+
+
+            current++;
+            return p;
+        }
+        return null;
+    }
+
+    public ProblemIterator getProblems() {
+        return new ProblemIterator() {
+            int index = 0;
+
+            public Problem next() {
+                Problem currentProb = problems.get(index);
+                index++;
+                return currentProb;
+            }
+
+            public boolean hasNext() {
+                return index < problems.size();
+            }
+        };
+    }
+
+    interface ProblemIterator {
+        Problem next();
+
+        boolean hasNext();
+    }
 }

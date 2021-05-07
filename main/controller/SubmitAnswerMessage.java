8464c769ffdd92e89d2a4c4cefeb2c3fe8bcfedd
DanNguyen2001 <78176995+DanNguyen2001@users.noreply.github.com>
2021-05-07 14:38:00 -0700

    Update SubmitAnswerMessage.java

--- main/controller/SubmitAnswerMessage.java
+++ main/controller/SubmitAnswerMessage.java
@@ -6,15 +6,26 @@ public class SubmitAnswerMessage implements Message{
 	private double answer;
 	private Problem prob;
 	
+	/**
+     	* ctor
+     	* @param answer given answer as a double
+     	* @param prob   given problem as a problem obj
+     	*/
 	public SubmitAnswerMessage(double answer, Problem prob){
 		this.answer = answer;
 		this.prob = prob;
 	}
 	
+	/**
+     	* @return answer as a double
+     	*/
 	public double submitAnswer() {
 		return answer;
 	}
 	
+	/**
+     	* @return problem as a problem object
+     	*/
 	public Problem getProblem() {
 		return prob;
 	}

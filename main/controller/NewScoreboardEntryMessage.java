d5161087d93cde534e0b7fb074d5a664a9806608
DanNguyen2001 <78176995+DanNguyen2001@users.noreply.github.com>
2021-05-07 14:36:21 -0700

    Update NewScoreboardEntryMessage.java

--- main/controller/NewScoreboardEntryMessage.java
+++ main/controller/NewScoreboardEntryMessage.java
@@ -6,15 +6,26 @@ public class NewScoreboardEntryMessage implements Message{
 	private String name;
 	private Score s;
 	
+	/**
+	* ctor
+     	* @param name given name as a String
+     	* @param s given score as a Score obj
+     	*/
 	public NewScoreboardEntryMessage(String name, Score s) {
 		this.name = name;
 		this.s = s;
 	}
 	
+	/**
+     	* @return score as a score obj
+    	*/
 	public Score getScore() {
 		return s;
 	}
 	
+	/**
+     	* @return name as a string
+     	*/
 	public String getName() {
 		return name;
 	}

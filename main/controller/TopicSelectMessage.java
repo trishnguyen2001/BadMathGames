99f3c3a27704e51210e44624686d12fdadcdf6ab
DanNguyen2001 <78176995+DanNguyen2001@users.noreply.github.com>
2021-05-07 14:38:49 -0700

    Update TopicSelectMessage.java

--- main/controller/TopicSelectMessage.java
+++ main/controller/TopicSelectMessage.java
@@ -3,10 +3,17 @@ package main.controller;
 public class TopicSelectMessage implements Message{
 	private String topic;
 	
+	/**
+     	* ctor
+     	* @param topic given topic as a string
+     	*/
 	public TopicSelectMessage(String topic){
 		this.topic = topic;
 	}
 	
+	/**
+     	* @return topic as a string
+     	*/
 	public String getTopic() {
 		return topic;
 	}
